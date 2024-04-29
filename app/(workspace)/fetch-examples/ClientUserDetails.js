07aa54d5b043d5cd92cf189e24ffa14181b2cda2
mech <mech@me.com>
2024-04-29 15:50:52 +0800

    2 kinds of tab

--- app/(workspace)/fetch-examples/ClientUserDetails.js
+++ app/(workspace)/fetch-examples/ClientUserDetails.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { IdentificationBadge } from "@phosphor-icons/react/dist/ssr";
-import { InfoIcon } from "lucide-react";
+import { InfoIcon, MessageSquareDashed, Sparkles, Shapes } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useQuery } from "urql";
@@ -49,45 +49,85 @@ export default function ClientUserDetails() {
   //     });
   // }, [id]);
 
-  return (
-    user && (
-      <div
-        className="space-y-4 p-4 data-[fetching=true]:opacity-50"
-        data-fetching={fetching}
-      >
-        <h1 className="flex items-center gap-2 text-lg font-semibold leading-tight">
-          <IdentificationBadge weight="fill" size={32} className="shrink-0" />{" "}
-          {user.name}
-        </h1>
+  if (fetching && user === undefined) {
+    return <div className="grid h-full place-content-center">Loading...</div>;
+  }
 
-        <div className="divide-y">
-          <div className="flex items-center justify-between gap-2 py-2">
-            <span className="text-gray-500">ID</span>
-            <span>{user.id}</span>
-          </div>
-          <div className="flex items-center justify-between gap-2 py-2">
-            <span className="text-gray-500">Name</span>
-            <strong>{user.name}</strong>
-          </div>
-          <div className="flex items-center justify-between gap-2 py-2">
-            <span className="text-gray-500">Email</span>
-            <a href={`mailto:${user.email}`} className="text-blue-700">
-              {user.email}
-            </a>
-          </div>
-          <div className="flex items-center justify-between gap-2 py-2">
-            <span className="flex items-center gap-1 text-gray-500">
-              Status
-              <InfoIcon size={18} />
-            </span>
-            <span>{user.status}</span>
-          </div>
-          <div className="flex items-center justify-between gap-2 py-2">
-            <span className="text-gray-500">Created</span>
-            {user.createdAt}
-          </div>
+  return (
+    <div
+      className="space-y-4 p-4 data-[fetching=true]:opacity-50"
+      data-fetching={fetching}
+    >
+      <h1 className="flex items-center gap-2 text-lg font-semibold leading-tight">
+        <IdentificationBadge weight="fill" size={32} className="shrink-0" />{" "}
+        {user.name}
+      </h1>
+
+      <div className="divide-y">
+        <div className="flex items-center justify-between gap-8 py-2">
+          <span className="text-gray-500">ID</span>
+          <span className="truncate">{user.id}</span>
+        </div>
+        <div className="flex items-center justify-between gap-8 py-2">
+          <span className="text-gray-500">Name</span>
+          <strong className="truncate">{user.name}</strong>
+        </div>
+        <div className="flex items-center justify-between gap-8 py-2">
+          <span className="text-gray-500">Email</span>
+          <a href={`mailto:${user.email}`} className="truncate text-blue-700">
+            {user.email}
+          </a>
+        </div>
+        <div className="flex items-center justify-between gap-2 py-2">
+          <span className="flex items-center gap-1 truncate text-gray-500">
+            Status
+            <InfoIcon size={18} />
+          </span>
+          <span>{user.status}</span>
+        </div>
+        <div className="flex items-center justify-between gap-2 py-2">
+          <span className="text-gray-500">Created</span>
+          <span className="truncate">{user.createdAt}</span>
         </div>
       </div>
-    )
+
+      <ul className="no-scrollbar flex items-center gap-8 overflow-y-hidden overflow-x-scroll overscroll-contain border-b pb-1">
+        <li>
+          <div className="flex flex-col items-center">
+            Tab 1<span className="h-1 w-3/5 rounded-full bg-blue-500"></span>
+          </div>
+        </li>
+        <li>Tab 2</li>
+        <li>Tab 3</li>
+      </ul>
+
+      <p className="rounded-lg bg-gray-50 p-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        architecto assumenda at atque dicta ea error excepturi, facere fugit id
+        impedit in iste, minus molestias necessitatibus repellat suscipit, velit
+        voluptatem.
+      </p>
+
+      <ul className="no-scrollbar -mx-4 flex cursor-pointer select-none flex-nowrap items-center gap-0 overflow-y-hidden overflow-x-scroll overscroll-contain border-y border-gray-300">
+        <li className="rounded-mds flex-none border-r border-gray-300 px-4 py-2 transition-colors hover:bg-gray-100">
+          <div className="flex items-center gap-1">
+            <MessageSquareDashed className="shrink-0" />
+            <span className="flex-none">Unread</span>
+          </div>
+        </li>
+        <li className="rounded-mds flex-none border-r border-gray-300 bg-gray-100 px-4 py-2 shadow-inner transition-colors">
+          <div className="flex flex-nowrap items-center gap-1">
+            <Sparkles className="shrink-0" />
+            <span className="flex-none">AI Assistant</span>
+          </div>
+        </li>
+        <li className="rounded-mds flex-none border-r border-gray-300 px-4 py-2 transition-colors hover:bg-gray-100">
+          <div className="flex items-center gap-1">
+            <Shapes className="shrink-0" />
+            <span className="flex-none">More Options...</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
